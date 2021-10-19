79cf5f10960752d63f5823536eda52760de051b3
Nuno Cunha <nunorafael_6@hotmail.com>
2021-10-19 23:55:00 +0100

    Updated dynamic unit

--- src/Components/ForecastPageComponents/WeatherComponent/index.tsx
+++ src/Components/ForecastPageComponents/WeatherComponent/index.tsx
@@ -25,7 +25,7 @@ export default class WeatherBlock extends React.Component<{className: string, we
                 <div className="weather-temperature">
                     <span className="weather-temperature-degree">{this.props.weather.temperature}</span>
                     <span className="weather-temperature-degree-type">°</span>
-                    <span className="weather-temperature-degree-type-scale">C</span>
+                    <span className="weather-temperature-degree-type-scale">{this.props.weather.unit[0]}</span>
                 </div>
                 <div className="weather-description">
                     <span className="weather-description-text">{this.props.weather.description}</span>
