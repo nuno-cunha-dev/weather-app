b50d654aaacaf9122b928985cecb016b87abab5b
Nuno Cunha <nunorafael_6@hotmail.com>
2021-10-19 01:08:40 +0100

    Request data from api

--- src/Components/ForecastPageComponents/WeatherComponent/index.tsx
+++ src/Components/ForecastPageComponents/WeatherComponent/index.tsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import weatherIcon from '../../../Assets/WeatherIcons/svg/wsymbol_0002_sunny_intervals.svg';
-import StyledComponentProp from '../../../Common/Interfaces/StyledComponentProp';
+import StyledComponentProp from '../../../Common/Interfaces/StyledComponentProp.interface';
 import './index.css';
 
-export default class WeatherBlock extends React.Component<StyledComponentProp, {}> {
+export default class WeatherBlock extends React.Component<StyledComponentProp | any, {}> {
+    private getWeekday(): string {
+        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        return days[ new Date().getDay() ];
+    }
+
     render() {
         return (
             <div className={"weather-block " + this.props.className}>
                 <div className="weather-date">
                     <div className="weather-day-of-year">Today</div>
-                    <div className="weather-day-of-week">Wednesday</div>
+                    <div className="weather-day-of-week">{this.getWeekday()}</div>
                 </div>
                 <div className="weather-icon">
                     <img src={weatherIcon} alt="Weather prediction today" />
                 </div>
                 <div className="weather-temperature">
-                    <span className="weather-temperature-degree">28</span>
+                    <span className="weather-temperature-degree">{this.props.weather.temperature}</span>
                     <span className="weather-temperature-degree-type">°</span>
                     <span className="weather-temperature-degree-type-scale">C</span>
                 </div>
                 <div className="weather-description">
-                    <span className="weather-description-text">Few clouds in</span>
-                    <span>&nbsp;</span>
-                    <span className="weather-description-city">Porto</span>
+                    <span className="weather-description-text">{this.props.weather.weatherDescription}</span>
+                    <span>&nbsp;in&nbsp;</span>
+                    <span className="weather-description-city">{this.props.weather.city}</span>
                 </div>
             </div>
         );
--- src/Pages/Forecast/index.tsx
+++ src/Pages/Forecast/index.tsx
@@ -1,9 +1,43 @@
+import axios from 'axios';
 import React from 'react';
+import WeatherDto from '../../Common/Dtos/Weather.dto';
 import WeatherBlock from '../../Components/ForecastPageComponents/WeatherComponent';
 import './index.css';
 
 export default class Forecast extends React.Component {
+    state = {
+        weatherData: {
+            upToDate: false,
+            temperature: 0,
+            weatherDescription: '',
+            city: '',
+        }
+    };
+
+    componentDidMount() {
+        if(this.state.weatherData.upToDate) {
+            return;
+        }
+        
+        axios.get('http://api.365weather.tk/weather/current')
+        .then((request: any) => {
+            this.buildWeatherBlockData(request.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+    private buildWeatherBlockData(weatherDto: WeatherDto) {
+        this.setState({ weatherData: {
+            upToDate: true,
+            temperature: weatherDto.temperature,
+            weatherDescription: weatherDto.description,
+            city: weatherDto.city,
+        }});
+    }
+
     render() {
-        return <WeatherBlock className="center-xy" />;
+        return <WeatherBlock className="center-xy" weather={this.state.weatherData}/>;
     }
 }
